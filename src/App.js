038fd0c95062f025f864b9002c40494543712d28
ay-sugiura <65064434+ay-sugiura@users.noreply.github.com>
2021-08-13 17:15:07 +0900

    再レンダリングの練習

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { memo, useMemo } from "react";
+import { useState, useCallback } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
 
+  const onClickOpen = () => setOpen(!open);
+
+  const temp = useMemo(() => 1 + 3, []);
+  const onClickClose = useCallback(() => setOpen(false), []);
   return (
     <div className="App">
-      <input />
+      <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button>表示</button>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
